2ef00352c9620a27933e5c4911ffd528cb9a91f7
Ratchagarn Naewbuntad <ratchagarn@omise.co>
2018-08-26 21:50:27 +0700

    Show current api url information.

--- src/features/Example03/Example03.js
+++ src/features/Example03/Example03.js
@@ -4,6 +4,7 @@
  * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
  */
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 import LayoutExamplePage from 'UI/LayoutExamplePage'
 import FetchJndWebApiData from 'modules/FetchJndWebApiData'
@@ -17,21 +18,27 @@ class Example03 extends Component {
 
     this.state = {
       selectedPage: 1,
+      currentApiUrl: '',
     }
   }
 
   render() {
-    const { selectedPage } = this.state
+    const { selectedPage, currentApiUrl } = this.state
     const params = {
       page: selectedPage,
     }
 
     return (
       <LayoutExamplePage title="Example 03 - Pagination" sourceName="Example03">
+        <CurrentApiUrl>
+          Current API URL:
+          <span>{currentApiUrl}</span>
+        </CurrentApiUrl>
         <FetchJndWebApiData
           path="campaigns"
           params={params}
           delay={250}
+          onSuccess={this.handleOnSuccess}
           render={(props) => (
             <Campaigns
               { ...props }
@@ -43,6 +50,14 @@ class Example03 extends Component {
     )
   }
 
+  handleOnSuccess = (response) => {
+    const { path, current_page } = response.data.meta
+
+    this.setState({
+      currentApiUrl: `${path}?page=${current_page}`
+    })
+  }
+
   handleOnPageChange = (data) => {
     const { selected } = data
     this.setState({ selectedPage: selected + 1 })
@@ -50,3 +65,17 @@ class Example03 extends Component {
 }
 
 export default Example03
+
+
+const CurrentApiUrl = styled.div`
+  margin-bottom: 20px;
+  font-size: 12px;
+  font-weight: bold;
+
+  > span {
+    margin-left: 4px;
+    color: purple;
+    font-weight: normal;
+    user-select: none;
+  }
+`
