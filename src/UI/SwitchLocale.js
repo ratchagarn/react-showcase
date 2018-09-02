0b24d492cba1d98c3e68430385e0a069114cce77
Ratchagarn Naewbuntad <ratchagarn@omise.co>
2018-09-03 00:56:31 +0700

    Update SwitchLocale style.

--- src/UI/SwitchLocale.js
+++ src/UI/SwitchLocale.js
@@ -9,15 +9,15 @@ import styled from 'styled-components'
 
 import { actionSetLocale } from 'redux/appReducer'
 
-const SwitchLocal = ({ dispatch }) => {
+const SwitchLocal = ({ locale, dispatch }) => {
   return (
     <Container>
-      <button onClick={onLocaleChange('EN')}>EN</button>
-      <button onClick={onLocaleChange('TH')}>TH</button>
+      <Locale active={locale === 'EN'} onClick={handleOnClick('EN')}>EN</Locale>
+      <Locale active={locale === 'TH'} onClick={handleOnClick('TH')}>TH</Locale>
     </Container>
   )
 
-  function onLocaleChange(locale) {
+  function handleOnClick(locale) {
     return () => {
       dispatch(actionSetLocale(locale))
       localStorage.setItem('locale', locale)
@@ -29,4 +29,27 @@ const Container = styled.div`
   margin-bottom: 20px;
 `
 
-export default connect()(SwitchLocal)
+const Locale = styled.span`
+  display: inline-block;
+  font-size: 12px;
+  color: ${props => props.active ? 'red' : '#333'};
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &:after {
+    content: '|';
+    display: inline-block;
+    margin-left: 4px;
+    margin-right: 4px;
+    color: #333;
+  }
+
+  &:last-child&:after {
+    display: none;
+  }
+`
+
+export default connect((state) => ({ locale: state.locale }))(SwitchLocal)
