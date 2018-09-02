36ac93f5703101ab3ea2f8f24448e7bc3b3a093d
Ratchagarn Naewbuntad <ratchagarn@omise.co>
2018-09-03 00:56:39 +0700

    Update Example 04

--- src/features/Example04/Example04.js
+++ src/features/Example04/Example04.js
@@ -4,23 +4,19 @@
  * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
  */
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
 import LayoutExamplePage from 'UI/LayoutExamplePage'
 
 
 class Example04 extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-    }
-  }
-
   render() {
     return (
       <LayoutExamplePage title="Example 04" sourceName="Example04">
+        <h1>Currect locale is <span style={{ color: 'red' }}>{this.props.locale}</span></h1>
       </LayoutExamplePage>
     )
   }
 }
 
-export default Example04
+export default connect((state) => ({ ...state }))(Example04)
